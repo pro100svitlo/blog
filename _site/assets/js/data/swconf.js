a685fc14f62d2ea8812b921f1c6823439c6105a1
Andrii Antonov <pro100svitlo@gmail.com>
2025-10-06 09:44:18 +0200

    chore: update _site

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1758656350',resources: [
+    cacheName: 'chirpy-1759736534',resources: [
       '/blog/assets/css/jekyll-theme-chirpy.css',
       '/blog/',
       
