3176525efce07b902a80bda2b70742a2444d659b
Andrii Antonov <pro100svitlo@gmail.com>
2026-03-04 09:37:47 +0100

    chore: build site

---
+++ _site/blog/assets/js/data/swconf.js
@@ -0,0 +1,1 @@
+const swconf = { cacheName: 'chirpy-1772613406',resources: [ '/blog/assets/css/jekyll-theme-chirpy.css', '/blog/', '/blog/categories/', '/blog/tags/', '/blog/archives/', '/blog/about/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };
