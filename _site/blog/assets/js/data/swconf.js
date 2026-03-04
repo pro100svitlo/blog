68a67ae42e0d1b90fa39e679a7661d2513987f3d
Andrii Antonov <pro100svitlo@gmail.com>
2026-03-04 10:19:21 +0100

    chore: update jekyll-theme-chirpy and code

--- _site/blog/assets/js/data/swconf.js
+++ _site/blog/assets/js/data/swconf.js
@@ -1,1 +1,1 @@
-const swconf = { cacheName: 'chirpy-1772615348',resources: [ '/blog/assets/css/jekyll-theme-chirpy.css', '/blog/', '/blog/categories/', '/blog/tags/', '/blog/archives/', '/blog/about/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };
+const swconf = { cacheName: 'chirpy-1772615941',resources: [ '/blog/assets/css/jekyll-theme-chirpy.css', '/blog/', '/blog/categories/', '/blog/tags/', '/blog/archives/', '/blog/about/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };
